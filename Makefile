2861f486aaa5b3d45d742b03f738c51da9a2ae38
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-04-29 12:12:37 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+SDSL_ROOT := ../sdsl-lite/build
+INCLUDEPATHS := -I$(SDSL_ROOT)/include -I$(SDSL_ROOT)/external/libdivsufsort/include
+LIBPATHS    := -L$(SDSL_ROOT)/lib -L$(SDSL_ROOT)/external/libdivsufsort/lib
+LIBS        := -lsdsl -ldivsufsort -ldivsufsort64
+
+CXX      := g++
+CXXFLAGS := -O2 -std=c++17 -Wall -Wno-sign-compare
+
+TARGET := fm_index_demo
+SRCS   := main.cpp FM_index.cpp
+
+$(TARGET): $(SRCS) FM_index.hh
+	$(CXX) $(CXXFLAGS) $(SRCS) -o $(TARGET) $(LIBPATHS) $(INCLUDEPATHS) $(LIBS)
+
+clean:
+	rm -f $(TARGET)
---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "FM_index.hh"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Index three DNA strings
+    vector<string> sequences = {
+        "ACGTACGTACGT",
+        "GATTACAGATTACA",
+        "ACGTTTTT",
+    };
+
+    FM_index fmi;
+    fmi.construct(sequences);
+
+    cout << "Indexed " << sequences.size() << " sequences, "
+         << "total BWT length: " << fmi.size() << "\n\n";
+
+    // Search for a few patterns and report all occurrences
+    vector<string> queries = {"ACGT", "GATTA", "TTTT", "ZZZZ"};
+
+    for (const string& q : queries) {
+        FM_index::Interval I = fmi.search(q);
+        if (I == fmi.get_empty_interval()) {
+            cout << "\"" << q << "\"  not found\n";
+            continue;
+        }
+        cout << "\"" << q << "\"  found " << I.size() << " time(s):\n";
+        for (LL rank = I.left; rank <= I.right; rank++) {
+            auto [doc, pos] = fmi.locate(rank);
+            if (doc >= 0)
+                cout << "  seq[" << doc << "] pos " << pos
+                     << "  -> \"" << sequences[doc].substr(pos, q.size()) << "\"\n";
+        }
+    }
+
+    return 0;
+}
