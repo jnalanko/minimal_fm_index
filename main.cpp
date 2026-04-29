0a9126aa60e5b321074ffe9c0ef80b368e414468
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-04-29 12:38:19 +0300

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -27,9 +27,9 @@ int main() {
         FM_index::Interval I = fmi.get_full_interval();
         for (int64_t i = (int64_t)q.size() - 1; i >= 0; i--) {
             I = fmi.left_extend(I, q[i]);
-            int64_t count = (I == fmi.get_empty_interval()) ? 0 : I.size();
+            int64_t count = I.size();
             cout << "  \"" << q.substr(i) << "\": " << count << "\n";
-            if (I == fmi.get_empty_interval()) break;
+            if (I.size() == 0) break;
         }
         cout << "\n";
     }
